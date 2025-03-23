9d88a210228fdb4a26070326443516ba191c4fa4
Ian M. Iori <ianmessia@gmail.com>
2025-03-23 16:03:18 -0300

    Dev: Added auth and sensors

---
+++ app/home.tsx
@@ -0,0 +1,24 @@
+import { View, Text, StyleSheet } from "react-native";
+import { useAuth } from "../context/auth";
+import { Link } from "expo-router";
+
+export default function Home() {
+  const { user } = useAuth();
+
+  return (
+    <View style={styles.container}>
+      <Text>Bem-vindo, {user.email}</Text>
+      <Link href="/sensors">Sensores</Link>
+      <Link href="/camera">Câmera</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+});
